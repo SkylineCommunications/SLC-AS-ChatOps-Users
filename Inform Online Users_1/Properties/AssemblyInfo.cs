6ae50a2bf2c0330dbdbd31941a26c4348cc8ee17
Joachim Ally <joachim.ally@skyline.be>
2023-06-15 13:18:05 +0200

    Also added the beginning of the second command we need to implement.

---
+++ Inform Online Users_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Inform Online Users_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Inform Online Users_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C85F0AEB-F471-4B53-B84E-7D2F0D3E0F27")]
+[assembly: AssemblyVersion("1.0.0.0")]
